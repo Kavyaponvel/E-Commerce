0122e8be73d4e29b08cc8222012bc0d2980baebe
Birundha-B <birundha9121@gmail.com>
2025-05-21 10:04:35 +0530

    Update orderRoutes.js

--- orderRoutes.js
+++ orderRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-// POST /api/orders
+
 router.post('/', (req, res) => {
   const { items, total } = req.body;
   console.log('📦 Order received:', req.body);
